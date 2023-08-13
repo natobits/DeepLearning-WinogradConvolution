1d43529f742fea98133849b538622f8dd630cf91
natobits <natobits@proton.me>
2023-08-13 19:20:00 +0000

    Day 2

--- github_winograd/src/winograd_test.cpp
+++ github_winograd/src/winograd_test.cpp
@@ -21,3 +21,38 @@ const int PRECISE = 0;
 #define KERNEL_INTEGER 1
 
 void testWinograd();
+
+int main() {
+
+
+	WINOGRAD_KERNEL::winograd2D_initialize();
+
+	testWinograd();
+
+	return 0;
+}
+
+void testWinograd() {
+
+	//int batch_size = 1;
+
+	int tiH = IH;
+	int tiW = IW;
+
+	int tkW = 3;
+	int tkH = 3;
+
+	int tsW = 1;
+	int tsH = 1;
+
+	int tiC = CIN;
+	const int toC = COUT;
+
+	bool tbias = true;
+
+	int tpad = 1;
+
+	const auto toH = (tiH + tpad * 2 - tkH) / tsH + 1;
+
+	// Output width.
+	const auto toW = (tiW + tpad * 2 - tkW) / tsW + 1;
